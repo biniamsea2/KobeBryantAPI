9f5fdefe8083ac1eb9db9a1d6dcfa13dba2d2fa0
Biniam <Biniamt23@yahoo.com>
2020-08-29 00:24:10 -0700

    db context working now

--- KobeBryantAPI/KobeBryantAPI/Data/KobeDBContext.cs
+++ KobeBryantAPI/KobeBryantAPI/Data/KobeDBContext.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using KobeBryantAPI.Model;
-using DbContext = Microsoft.EntityFrameworkCore.DbContext;
 
 namespace KobeBryantAPI.Data
 {
