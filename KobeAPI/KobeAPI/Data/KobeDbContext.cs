bd341db443db035984b3197d1cd9d4abb05d842b
Biniam <Biniamt23@yahoo.com>
2020-09-01 21:02:23 -0700

    Clean repo

--- KobeAPI/KobeAPI/Data/KobeDbContext.cs
+++ KobeAPI/KobeAPI/Data/KobeDbContext.cs
@@ -1,15 +1,15 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
 
 namespace KobeAPI.Data
 {
     public class KobeDbContext : DbContext
     {
-        public KobeDbContext(DbContextOptions<KobeDbContext> options) : base(options))
+        public KobeDbContext(DbContextOptions<KobeDbContext> options) : base(options)
         {
 
         }
